d6375ea54f370c2a565ed77e1fefdba646c6b338
Great-Stone <hahohh@gmail.com>
2023-09-12 13:01:12 +0900

    thymeleaf 설정 변경

--- java-app/src/main/java/com/example/demo/VaultDataController.java
+++ java-app/src/main/java/com/example/demo/VaultDataController.java
@@ -16,164 +16,164 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.ui.Model;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
-
-import com.example.demo.RawData;
-import com.example.demo.RawDataRepository;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VaultDataController {
-    @Autowired
-    private VaultDataRepository vaultDataRepository;
+  @Autowired
+  private VaultDataRepository vaultDataRepository;
 
-    @Autowired
-    private RawDataRepository rawDataRepository;
+  @Autowired
+  private RawDataRepository rawDataRepository;
 
-    @Autowired
-    private VaultService vaultService;
+  @Autowired
+  private VaultService vaultService;
 
-    @GetMapping("/")
-    public String getAllData(Model model) {
-        Iterable<VaultData> vaultData = vaultDataRepository.findAll();
-        Iterable<RawData> rawData = rawDataRepository.findAll();
+  @GetMapping("/")
+  public String getAllData(Model model) {
+    Iterable<VaultData> vaultData = vaultDataRepository.findAll();
+    Iterable<RawData> rawData = rawDataRepository.findAll();
 
-        model.addAttribute("vault_data", vaultData);
-        model.addAttribute("raw_data", rawData);
-        model.addAttribute("newData", new VaultData()); // 빈 데이터 객체
-        return "/dataList.html"; // Thymeleaf 템플릿 이름
-    }
+    model.addAttribute("vault_data", vaultData);
+    model.addAttribute("raw_data", rawData);
+    model.addAttribute("newData", new VaultData()); // 빈 데이터 객체
+    return "dataList.html"; // Thymeleaf 템플릿 이름
+  }
 
-    @PostMapping("/insertData")
-    public String insertData(VaultData newData) {
-        vaultDataRepository.save(newData);
-        return "redirect:/"; // 다시 데이터 목록으로 리다이렉트
-    }
+  @PostMapping("/insertData")
+  public String insertData(VaultData newData) {
+    vaultDataRepository.save(newData);
+    return "redirect:/"; // 다시 데이터 목록으로 리다이렉트
+  }
 
-    @PostMapping("/uploadEncryptAndSave")
-    public String uploadEncryptAndSave(@RequestParam("file") MultipartFile file) {
-        // 여기에 파일 처리 로직을 추가
-        if (!file.isEmpty()) {
-          try {
-            // // 파일 저장
-            String FilePath = "/Users/gs/Downloads/" + file.getOriginalFilename();
+  @PostMapping("/uploadEncryptAndSave")
+  public String uploadEncryptAndSave(@RequestParam("file") MultipartFile file) {
+    // 여기에 파일 처리 로직을 추가
+    if (!file.isEmpty()) {
+      try {
+        // // 파일 저장
+        String FilePath = "/Users/gs/Downloads/" + file.getOriginalFilename();
 
-            String tempFilePath = FilePath + ".tmp";
-            file.transferTo(new File(tempFilePath));
+        String tempFilePath = FilePath + ".tmp";
+        file.transferTo(new File(tempFilePath));
 
-            // // 파일 읽기
-            FileInputStream fileInputStream = new FileInputStream(tempFilePath);
-            byte[] fileData = fileInputStream.readAllBytes();
-            fileInputStream.close();
+        // // 파일 읽기
+        FileInputStream fileInputStream = new FileInputStream(tempFilePath);
+        byte[] fileData = fileInputStream.readAllBytes();
+        fileInputStream.close();
 
-            // 파일 데이터 암호화
-            byte[] encryptedData = vaultService.encryptData(fileData);
+        // 파일 데이터 암호화
+        byte[] encryptedData = vaultService.encryptData(fileData);
 
-            // 암호화된 데이터 저장
-            String encryptedFilePath = FilePath + ".enc";
-            FileOutputStream encryptedFileOutputStream = new FileOutputStream(encryptedFilePath);
-            encryptedFileOutputStream.write(encryptedData);
-            encryptedFileOutputStream.close();
-            
-          } catch (IOException e) {
-            e.printStackTrace();
-          }
-        } else {
-
-        }
-        return "redirect:/"; // 다시 데이터 목록으로 리다이렉트
-    }
-
-    @PostMapping("/uploadEncrypt")
-    public ResponseEntity<InputStreamResource> uploadEncrypte(@RequestParam("file") MultipartFile file) {
-        // 여기에 파일 처리 로직을 추가
-        if (!file.isEmpty()) {
-          try {
-            // // 파일 읽기
-            byte[] fileData = file.getBytes();
-
-            // Base64 Encoding
-            byte[] encoded = Base64.getEncoder().encode(fileData);
-
-            // 파일 데이터 암호화
-            byte[] encryptedData = vaultService.encryptData(encoded);
-
-            // 바이트 배열을 사용하여 새로운 FileInputStream 생성
-            File tempFile = File.createTempFile("encrypted", ".txt"); // 임시 파일 생성
-            FileOutputStream fileOutputStream = new FileOutputStream(tempFile);
-            fileOutputStream.write(encryptedData);
-            fileOutputStream.close();
-
-            // FileInputStream fileInputStream = new FileInputStream(tempFile);
-
-            // 바이트 배열을 사용하여 새로운 FileInputStream 생성
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
-            headers.setContentDispositionFormData("attachment", tempFile.getName());
-
-            System.out.println(tempFile.getName());
-
-            InputStreamResource resource = new InputStreamResource(new FileInputStream(tempFile));
-
-            return ResponseEntity.ok()
-                    .headers(headers)
-                    .contentLength(tempFile.length())
-                    .body(resource);
-            
-          } catch (IOException e) {
-            e.printStackTrace();
-          }
-        }
+        // 암호화된 데이터 저장
+        String encryptedFilePath = FilePath + ".enc";
+        FileOutputStream encryptedFileOutputStream = new FileOutputStream(encryptedFilePath);
+        encryptedFileOutputStream.write(encryptedData);
+        encryptedFileOutputStream.close();
         
-        return ResponseEntity.notFound().build(); // 파일이 없거나 처리에 실패한 경우 404 응답 반환
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    } else {
+      System.out.println("FileEmpty:" + file.getOriginalFilename());
     }
+    return "redirect:/"; // 다시 데이터 목록으로 리다이렉트
+  }
 
-    @PostMapping("/uploadDecrypt")
-    public ResponseEntity<InputStreamResource> uploadDecrypt(@RequestParam("file") MultipartFile file) {
-        // 여기에 파일 처리 로직을 추가
-        if (!file.isEmpty()) {
-          try {
-            // // 파일 읽기
-            byte[] fileData = file.getBytes();
+  @PostMapping("/uploadEncrypt")
+  public ResponseEntity<InputStreamResource> uploadEncrypte(@RequestParam("file") MultipartFile file) {
+    // 여기에 파일 처리 로직을 추가
+    if (!file.isEmpty()) {
+      try {
+        // // 파일 읽기
+        byte[] fileData = file.getBytes();
 
-            // 파일 데이터 복호화
-            byte[] decryptedData = vaultService.decryptData(fileData);
+        // Base64 Encoding
+        byte[] encoded = Base64.getEncoder().encode(fileData);
 
-            // Base64 Decoding
-            byte[] decoded = Base64.getDecoder().decode(decryptedData);
+        // 파일 데이터 암호화
+        byte[] encryptedData = vaultService.encryptData(encoded);
 
-            // 바이트 배열을 사용하여 새로운 FileInputStream 생성
-            File tempFile = File.createTempFile("decrypted", ".txt"); // 임시 파일 생성
-            FileOutputStream fileOutputStream = new FileOutputStream(tempFile);
-            fileOutputStream.write(decoded);
-            fileOutputStream.close();
+        // 바이트 배열을 사용하여 새로운 FileInputStream 생성
+        File tempFile = File.createTempFile("encrypted", ".txt"); // 임시 파일 생성
+        FileOutputStream fileOutputStream = new FileOutputStream(tempFile);
+        fileOutputStream.write(encryptedData);
+        fileOutputStream.close();
 
-            // FileInputStream fileInputStream = new FileInputStream(tempFile);
 
-            // 바이트 배열을 사용하여 새로운 FileInputStream 생성
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
-            headers.setContentDispositionFormData("attachment", tempFile.getName());
+        // 바이트 배열을 사용하여 새로운 FileInputStream 생성
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+        headers.setContentDispositionFormData("attachment", tempFile.getName());
 
-            System.out.println(tempFile.getName());
+        System.out.println(tempFile.getName());
 
-            InputStreamResource resource = new InputStreamResource(new FileInputStream(tempFile));
+        FileInputStream fileInputStream = new FileInputStream(tempFile);
+        InputStreamResource resource = new InputStreamResource(fileInputStream);
 
-            return ResponseEntity.ok()
-                    .headers(headers)
-                    .contentLength(tempFile.length())
-                    .body(resource);
-            
-          } catch (IOException e) {
-            e.printStackTrace();
-          }
-        }
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentLength(tempFile.length())
+                .body(resource);
         
-        return ResponseEntity.notFound().build(); // 파일이 없거나 처리에 실패한 경우 404 응답 반환
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
     }
+    
+    return ResponseEntity.notFound().build(); // 파일이 없거나 처리에 실패한 경우 404 응답 반환
+  }
+
+  @PostMapping("/uploadDecrypt")
+  public ResponseEntity<InputStreamResource> uploadDecrypt(@RequestParam("file") MultipartFile file) {
+    // 여기에 파일 처리 로직을 추가
+    if (!file.isEmpty()) {
+      try {
+        // // 파일 읽기
+        byte[] fileData = file.getBytes();
+
+        // 파일 데이터 복호화
+        byte[] decryptedData = vaultService.decryptData(fileData);
+
+        // Base64 Decoding
+        byte[] decoded = Base64.getDecoder().decode(decryptedData);
+
+        // 바이트 배열을 사용하여 새로운 FileInputStream 생성
+        File tempFile = File.createTempFile("decrypted", ".txt"); // 임시 파일 생성
+        FileOutputStream fileOutputStream = new FileOutputStream(tempFile);
+        fileOutputStream.write(decoded);
+        fileOutputStream.close();
+
+        // FileInputStream fileInputStream = new FileInputStream(tempFile);
+
+        // 바이트 배열을 사용하여 새로운 FileInputStream 생성
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+        headers.setContentDispositionFormData("attachment", tempFile.getName());
+
+        System.out.println(tempFile.getName());
+
+        InputStreamResource resource = new InputStreamResource(new FileInputStream(tempFile));
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentLength(tempFile.length())
+                .body(resource);
+        
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    } else {
+      System.out.println("FileEmpty:" + file.getOriginalFilename());
+    }
+    
+    return ResponseEntity.notFound().build(); // 파일이 없거나 처리에 실패한 경우 404 응답 반환
+  }
 }
